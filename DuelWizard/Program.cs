42807c8f9a980e8fab0ef678cc67f2052360310a
franysia <francobocilff@gmail.com>
2026-02-09 10:38:52 +0700

    Update Program.cs

--- DuelWizard/Program.cs
+++ DuelWizard/Program.cs
@@ -1,54 +1,96 @@
 ﻿using System.Security.Cryptography.X509Certificates;
 Wizard wizardA = new Wizard("Gojo", 20);
-Wizard wizardB = new Wizard("sukuna", 30);
+Wizard wizardB = new Wizard("sukuna" , 20);
 
-Console.WriteLine("Pertarungan Dimulai!!!\n");
+
+Console.WriteLine("Permainan Dimulai...\n");
 wizardA.ShowStats();
 wizardB.ShowStats();
 
-wizardA.Attack(wizardB);
-wizardB.Attack(wizardA);
-wizardA.Attack(wizardB);
-wizardB.Attack(wizardA);
 
-Console.WriteLine("Pertarungan Berakhir!!!\n");
+string pilihan;
+
+while (wizardA.Energi > 0 && wizardB.Energi > 0)
+{
+Console.WriteLine($"1. {wizardA.Name} menyerang {wizardB.Name}");
+Console.WriteLine($"2. {wizardB.Name} menyerang {wizardA.Name}");
+Console.WriteLine($"3. {wizardA.Name} melakukan heal");
+Console.WriteLine($"4. {wizardB.Name} melakukan heal");
+
+Console.WriteLine("\nMasukkan pilihan anda (1/2/3/4): ");
+pilihan = Console.ReadLine();
+
+if (pilihan == "1")
+{
+wizardA.Attack(wizardB);
+}
+else if (pilihan == "2")
+{
+wizardB.Attack(wizardA);
+}
+else if (pilihan == "3")
+{
+wizardA.Heal();
+}
+else if (pilihan == "4")
+{
+wizardB.Heal();
+}
+else
+{
+        Console.WriteLine("Pilihan anda kocak");
+}
+}
+
+Console.WriteLine("Permainan berakhir\n");
 wizardA.ShowStats();
 wizardB.ShowStats();
+
 public class Wizard
 {
+    
     public string Name;
-    public int Energy;
+    public int Energi;
     public int Damage;
 
-
-    // deklarasi construktor
-    public Wizard(string name, int damage)
+    
+    public Wizard(string nama, int damage)
     {
-        Name = name;
-        Energy = 100;
+        Name = nama;
+        Energi = 50;
         Damage = damage;
-       
-
 
     }
-
-
 
     public void ShowStats()
     {
         Console.WriteLine("Statistik Wizard");
-        Console.WriteLine($"Nama: {Name}, Energy: {Energy} \n");
-
-       
+        Console.WriteLine($"Nama: {Name}, Energi: {Energi} \n");
     }
 
-    public void Attack(Wizard wizardLawanobj)
+    public void Attack(Wizard wizardLawanOBJ)
     {
-        wizardLawanobj.Energy -= Energy;
-        Console.WriteLine($"{Name} Menyerang{wizardLawanobj}");
-        Console.WriteLine($"Sisa Energi {wizardLawanobj.Name} adalah {wizardLawanobj.Energy} \n");
+        //mengurangi energi wizarLawanOBJ sebesar damage
+        wizardLawanOBJ.Energi -= Damage;
+        Console.WriteLine($"{Name} menyerang {wizardLawanOBJ.Name}");
+        Console.WriteLine($"sisa energi {wizardLawanOBJ.Name} adalah {wizardLawanOBJ.Energi}");
     }
 
+    public void Heal()
+    {
+        Energi += 5;
 
+        //menambah energi
+        if (Energi <= 100)
+        {
 
+            Console.WriteLine($"{Name} melakukan heal! Energi meningkat menjadi {Energi}");
+        }
+        else
+        {
+            Energi = 100;
+            Console.WriteLine("Sudah mencapai energi maksimum!");
+        }
+
+    }
 }
